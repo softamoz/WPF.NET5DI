59e43d9632b0a51f6933788d22a173b5bef10b43
Serajoddin Aliabadi <serajoddin.aliabadi@yahoo.com>
2021-04-12 15:45:23 +0430

    Test Configuration with Di and GetConnectionString Method

--- src/WPF.NET5DIAndConfiguration/WPF.NET5DIAndConfiguration/MainWindow.xaml.cs
+++ src/WPF.NET5DIAndConfiguration/WPF.NET5DIAndConfiguration/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Microsoft.Extensions.Configuration;
 
 namespace WPF.NET5DIAndConfiguration
 {
@@ -20,9 +8,10 @@ namespace WPF.NET5DIAndConfiguration
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow(IConfiguration configuration)
         {
             InitializeComponent();
+            var connectionString = configuration.GetConnectionString("Default");
         }
     }
 }
